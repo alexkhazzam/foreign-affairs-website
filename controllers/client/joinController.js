d2c12ce639a996257520e9b0326ea1d69e3d460c
alexkhazzam <alex722khazzam@gmail.com>
2020-10-13 14:16:49 -0400

    adding media queries for register and login page

--- controllers/client/joinController.js
+++ controllers/client/joinController.js
@@ -3,13 +3,11 @@ let location;
 exports.getJoinPage = (req, res, next) => {
   res.render('client/join', {
     location:
-      req.query.locationSearched === true
-        ? false
-        : 'Great Neck North High School',
+      req.query.locationSearched === 'success' ? location : 'gn north high',
   });
 };
 
 exports.postJoinPage = (req, res, next) => {
   location = req.body.searchedLocation;
-  res.redirect('/join/?locationSearched=true');
+  res.redirect('/join/?locationSearched=success');
 };
