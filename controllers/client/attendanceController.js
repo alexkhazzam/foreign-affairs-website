75032e37a69010c4b5b13779e32f0693d4eb8be4
alexkhazzam <alex722khazzam@gmail.com>
2020-10-18 21:41:30 -0400

    attendance logic

--- controllers/client/attendanceController.js
+++ controllers/client/attendanceController.js
@@ -1,12 +1,17 @@
 exports.getAttendancePage = (req, res, next) => {
-  res.render('client/attendance/currentYear', {});
+  res.render('client/attendance/years/fall-2020', { currYear: 'Fall - 2020' });
 };
 
 exports.getAttendanceYear = (req, res, next) => {
-  console.log(req.params.yearId);
-  res.render(
-    `client/attendance/years/${req.params.yearId.split('-')[0].split(':')[1]}-${
-      req.params.yearId.split('-')[1]
-    }`
-  );
+  let currentYear = `${req.params.yearId.split('-')[0].split(':')[1]}-${
+    req.params.yearId.split('-')[1]
+  }`;
+  res.render(`client/attendance/years/${currentYear}`, {
+    currYear: `${currentYear
+      .split('-')[0]
+      .charAt(0)
+      .toUpperCase()}${currentYear.split('-')[0].slice(1)} ${
+      currentYear.split('-')[1]
+    }`,
+  });
 };
