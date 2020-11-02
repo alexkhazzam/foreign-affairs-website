6fd0256fa869ebaef6e37f39942b92cd7dbd54e4
alexkhazzam <alex722khazzam@gmail.com>
2020-11-02 08:55:46 -0500

    working on member information page

--- controllers/client/MemberController.js
+++ controllers/client/MemberController.js
@@ -1,6 +1,7 @@
 const memberInformationModel = require('../../models/client/memberInformation/memberInformationModel');
 
 let memberList;
+let submitted;
 
 exports.getMemberPage = (req, res, next) => {
   const name = req.params.member;
@@ -11,11 +12,13 @@ exports.getMemberPage = (req, res, next) => {
 exports.getMemberInformationPage = (req, res, next) => {
   res.render('client/memberInformation', {
     members: req.query.membersFound === 'success' ? memberList : false,
+    submitted: submitted,
   });
 };
 
 exports.postMemberInformationPage = (req, res, next) => {
   const memberName = req.body.memberName;
+  submitted = memberName;
   const memberInfo = new memberInformationModel.MemberInformationModel(
     memberName
   );
