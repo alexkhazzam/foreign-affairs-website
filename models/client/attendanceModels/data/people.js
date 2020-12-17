09439a18d302f268ec5e68f0cd5faa6efe098b2a
alexkhazzam <alex722khazzam@gmail.com>
2020-12-16 21:10:12 -0500

    adding stuff

---
+++ models/client/attendanceModels/data/people.js
@@ -0,0 +1,17 @@
+/**
+ * Jonathan Moalemi
+ * Leon Asnadi
+ * Alexander Khazzam
+ * Ilan Rastegar
+ * Ethan Kane
+ * Liam-Kashinejad
+ * Dylan-Yaghoutiel
+ * Brian-Asnadi
+ * Debra-Davidson
+ * Joseph-Edelmen
+ * Kalyn-Jones
+ * Remi-Gilardian
+ * Alice-Yang
+ * Andrew-Suh
+ * Brandon-Tehrani
+ */
