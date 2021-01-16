667e6d6f10f40628a391d4a682d197f28d0eb638
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-01-15 20:35:59 -0500

    fetching ip

--- controllers/client/homepageController.js
+++ controllers/client/homepageController.js
@@ -2,38 +2,36 @@ const email = require('../../models/client/contactModel');
 const superagent = require('superagent');
 
 exports.getHomepage = (req, res, next) => {
-  let failedReq = false;
-  superagent.get('https://api.ipify.org/?format=json').then((data) => {
-    superagent
-      .get(
-        `http://api.ipstack.com/${data.body.ip}?access_key=5bce7537594b52d327777315eece71e9`
-      )
-      .then((info) => {
-        console.log(info.body);
-      });
-    res.render('client/homepage', {
-      ip: data.body.ip,
+  superagent
+    .get('https://api.ipify.org/?format=json')
+    .then((data) => {
+      superagent
+        .get(
+          `http://api.ipstack.com/${data.body.ip}?access_key=${process.env.STACKKEY}`
+        )
+        .then((ipInf) => {
+          const Email = new email.contactModel(
+            null,
+            null,
+            null,
+            null,
+            'website-pinged',
+            JSON.stringify(ipInf.body)
+          );
+          Email.sendEmail().then((data) => {
+            console.log(data);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          throw err;
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
     });
-    if (!data) {
-      failedReq = true;
-    }
-  });
-  if (failedReq) {
-    res.render('client/homepage', {
-      ip: '|',
-    });
-  }
-
-  // const Email = new email.contactModel(
-  //   null,
-  //   null,
-  //   null,
-  //   null,
-  //   'website-pinged'
-  // );
-  // Email.sendEmail().then((data) => {
-  //   console.log(data);
-  // });
+  res.render('client/homepage', {});
 };
 
 exports.getSettings = (req, res, next) => {
